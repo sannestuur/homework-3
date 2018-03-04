913040969ca9067efb8ca4e71e28774b4a326f29
Sanne Stuur <sanne.stuur@gmail.com>
2018-03-04 08:35:32 +0100

    initialized project

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+
+app.listen(4001, () => console.log('Express API listening on port 4001'))
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+  next()
+})
+
+const Event = sequelize.define('event', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  start_date: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  end_date: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+ {
+  tableName: 'events',
+  timestamps: false
+})
